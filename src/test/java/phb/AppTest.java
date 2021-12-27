2e31a32a0bd0550ac38b6451a87a636f63f2ebfe
phbndcl <polisetty.harish@gmail.com>
2021-12-27 12:26:43 +0530

    Maven

---
+++ src/test/java/phb/AppTest.java
@@ -0,0 +1,20 @@
+package phb;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
